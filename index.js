77e15917d94a12060084518e3cfc944569d19af2
mrlee23 <mrlee_23@naver.com>
2017-12-26 11:39:04 +0900

    Update js-doc in index.js

--- index.js
+++ index.js
@@ -1,23 +1,92 @@
+/**
+ * @fileOverview Manipulate project directories and files.
+ * @name index.js
+ * @author Dongsoo Lee <mrlee_23@naver.com>
+ * @copyright 2017 Dongsoo Lee <mrlee_23@naver.com>
+ * @module index
+ * @version 0.0.1
+ * @since 0.0.1
+ * @created 2017-12-26
+ *
+ * @requires module:./lib/typeCheck.js
+ * @requires module:./lib/path.js
+ */
+/**
+ * @typedef {string|RegExp|Array} query
+ */
+
 const type = require('./lib/typeCheck.js'),
 	  path = require('./lib/path.js');
 
+/**
+ * @class
+ * @name ProjectDir
+ * @classdesc project-dir main class
+ * @version 0.0.1
+ * @since 0.0.1
+ * @created 2017-12-26
+ */
 class ProjectDir {
+	/**
+	 * @public
+	 * @instance
+	 * @function constructor
+	 * @version 0.0.1
+	 * @since 0.0.1
+	 * @created 2017-12-26
+	 * @memberof module:index~ProjectDir
+	 * @description Generating new project directory object.
+	 *
+	 * @param {string} currentPath - Starting point for search base directory. This path must be an existing file or directory.
+	 * @param {query} basename - Dominating file or directory names.
+	 *
+	 * @example
+	 * .constructor('./', 'node_modules')
+	 */
 	constructor(currentPath, basename) {
 		(basename == null) && (this.basename = [".git", "package.json"]);
 		this.basename = basename;
 		this.basedir = currentPath;
 	}
 
-	_setBase () {
-		let basename = this.basename;
-	}
-
+	/**
+	 * @public
+	 * @instance
+	 * @function setbasename
+	 * @version 0.0.1
+	 * @since 0.0.1
+	 * @created 2017-12-26
+	 * @memberof module:index~ProjectDir
+	 * @description Set dominating file or directory names.
+	 *
+	 * @param {query} basename - Dominating file or directory names.
+	 * @throws {Error} Not a query type.
+	 *
+	 * @example
+	 * .setbasename(['.git', 'package.json'])
+	 */
 	set basename (basename) {
 		if (!type.isQuery(basename)) throw new Error(`basename(${basename}) is not a query type.`);
 		this._basename = basename;
 	}
 	get basename () { return this._basename; }
 
+	/**
+	 * @public
+	 * @instance
+	 * @function setbasedir
+	 * @version 0.0.1
+	 * @since 0.0.1
+	 * @created 2017-12-26
+	 * @memberof module:index~ProjectDir
+	 * @description Set project's base directory.
+	 *
+	 * @param {string} _path - Strting point for search base directory.
+	 * @throws {Error} Failed searching base directory of project.
+	 *
+	 * @example
+	 * .setbasedir('./')
+	 */
 	set basedir (_path) {
 		type.isFile(_path) && (_path = path.dirname(_path));
 		if (!type.isDir(_path)) throw new Error(`_path(${_path}) does not exist directory.`);
@@ -36,12 +105,44 @@ class ProjectDir {
 	}
 	get basedir () { return this._basedir; }
 
+	/**
+	 * @public
+	 * @instance
+	 * @function setwd
+	 * @version 0.0.1
+	 * @since 0.0.1
+	 * @created 2017-12-26
+	 * @memberof module:index~ProjectDir
+	 * @description Set working directory based on current project's base directory.
+	 *
+	 * @param {string} _path - Sub path of base directory.
+	 *
+	 * @example
+	 * .setwd('abcdef')
+	 */
 	set wd (_path) {
 		let wd = this.resolve(_path);
 		wd != null && (this._workingDir = wd);
 	}
 	get wd () { return this._workingDir || this.basedir; }
 
+	/**
+	 * @public
+	 * @instance
+	 * @function resolve
+	 * @version 0.0.1
+	 * @since 0.0.1
+	 * @created 2017-12-26
+	 * @memberof module:index~ProjectDir
+	 * @description Resolving the path based on current project's base directory.
+	 *
+	 * @param {string} _path - to resolve path.
+	 * @throws {Error} _path is not a string type.
+	 * @returns {string} resolved path.
+	 *
+	 * @example
+	 * .resolve('/abcd/efgh')
+	 */
 	resolve (_path) {
 		if (typeof _path !== 'string') throw new Error(`_path(${_path}) is not a string type.`);
 		if (type.isAbs(_path)) {
@@ -58,6 +159,23 @@ class ProjectDir {
 		return _path;
 	}
 
+	/**
+	 * @public
+	 * @instance
+	 * @function parse
+	 * @version 0.0.1
+	 * @since 0.0.1
+	 * @created 2017-12-26
+	 * @memberof module:index~ProjectDir
+	 * @description Parsing a path.
+	 *
+	 * @param {string} _path - to parse path.
+	 * @throws {Error} _path is not a string type.
+	 * @returns {Object} parsed path's object.
+	 *
+	 * @example
+	 * .parse('abcd')
+	 */
 	parse (_path) {
 		if (typeof _path !== 'string') throw new Error(`_path(${_path}) is not a string type.`);
 		let rPath = this.resolve(_path);
