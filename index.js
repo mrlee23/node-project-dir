2660cfccca53da5a40ff98ae36da1fa2420cb90b
mrlee23 <mrlee_23@naver.com>
2017-12-26 09:48:08 +0900

    Add resolve, wd function.
    The resolve function parses path based on base directory of project.

--- index.js
+++ index.js
@@ -21,12 +21,42 @@ class ProjectDir {
 	set basedir (_path) {
 		type.isFile(_path) && (_path = path.dirname(_path));
 		if (!type.isDir(_path)) throw new Error(`_path(${_path}) does not exist directory.`);
-		let basedir = path.locateBase(_path, this.basename);
-		if (path.equal(basedir, "/")) throw new Error(`_path(${_path}) has no parent node name of basename(${this.basename})`);
+		if (type.isRoot(_path)) throw new Error(`_path(${_path}) cannot be a root.`);
+		let basedir;
+		if (Array.isArray(this.basename)) {
+			this.basename.some(name => {
+				let tmpLoc = path.locateBase(_path, name);
+				!type.isRoot(tmpLoc) && (basedir = tmpLoc);
+			});
+		} else {
+			basedir = path.locateBase(_path, this.basename);
+		}
+		if (type.isRoot(basedir) || basedir == null) throw new Error(`_path(${_path}) has no parent node name of basename(${this.basename})`);
 		this._basedir = basedir;
 	}
-
 	get basedir () { return this._basedir; }
+
+	set wd (_path) {
+		this._workingDir = this.resolve(_path);
+	}
+	get wd () { return this._workingDir; }
+
+	resolve (_path) {
+		if (typeof _path !== 'string') throw new Error(`_path(${_path}) is not a string type.`);
+		if (type.isAbs(_path)) {
+			_path = path.resolve(this.basedir, _path.substr(1));
+		} else {
+			_path = path.resolve(this.wd || this.basedir, _path);
+		}
+
+		// out of project directory.
+		if (!path.equal(this.basedir, _path) &&
+			!path.isParent(this.basedir, _path)) 
+			return null;
+		
+		return _path;
+	}
+
 }
 
 module.exports = ProjectDir;
