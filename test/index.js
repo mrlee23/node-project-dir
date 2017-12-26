b69f2897a8b7063134d6e5b20ffcd2b881991fa0
mrlee23 <mrlee_23@naver.com>
2017-12-26 09:55:40 +0900

    Add resolve, wd unit test

--- test/index.js
+++ test/index.js
@@ -2,28 +2,91 @@ const genMocha = require('one-mocha'),
 	  path = require('path'),
 	  index = require('../index.js');
 
-let projectDir = new index("./", "node_modules");
+let projectDir = new index(path.resolve(__dirname, 'test-project'), "node_modules");
 class indexTester {
 	test () {
 		genMocha(
 			[
 				{
-					method: () => { return projectDir.basename; },
-					name: 'get basename',
+					method: (arg) => { projectDir.basename = arg;
+									   return projectDir.basename;
+									 },
+					name: 'get/set basename',
+					test:
+					[
+						{
+							assert: 'doesNotThrow',
+							args: [["node_modules", undefined],
+								   [["node_modules", ".git", "Makefile"], undefined],
+								   [["node_modules"], undefined]]
+						},
+						{
+							assert: 'throws',
+							args: [[1, Error],
+								   [[1], Error],
+								   [{}, Error]]
+						},
+						{
+							assert: 'equal',
+							args: [["node_modules", "node_modules"]]
+						},
+						{
+							assert: 'deepEqual',
+							args: [[["node_modules", ".git", "Makefile"], ["node_modules", ".git", "Makefile"]]]
+						}
+					]
+				},
+				{
+					method: (arg) => { projectDir.basedir = arg;
+									   return projectDir.basedir;
+									 },
+					name: 'get/set basedir',
+					test:
+					[
+						{
+							assert: 'doesNotThrow',
+							args: [[path.resolve(__dirname, 'test-project'), undefined]
+								  ]
+						},
+						{
+							assert: 'throws',
+							args: [["/", Error]]
+						},
+						{
+							assert: 'equal',
+							args: [[path.resolve(__dirname, "test-project"), path.resolve(__dirname, "test-project")],
+								   [path.resolve(__dirname, "test-project/sample"), path.resolve(__dirname, "test-project") + "/"],
+								   [path.resolve(__dirname, "test-project/README.md"), path.resolve(__dirname, "test-project")]]
+						}
+					]
+				},
+				{
+					method: projectDir.resolve,
+					name: 'resolve',
+					this: projectDir,
 					test: {
 						assert: 'equal',
-						args: [["node_modules"]]
+						args: [["/", path.resolve(__dirname, 'test-project')],
+							   ["/abcdef", path.resolve(__dirname, 'test-project/abcdef')],
+							   ["abcdef", path.resolve(__dirname, 'test-project/abcdef')],
+							   ["./", path.resolve(__dirname, 'test-project')],
+							   ["../", null]]
 					}
 				},
 				{
-					method: () => { return projectDir.basedir; },
-					name: 'get basedir',
+					method: (arg) => { projectDir.wd = arg;
+									   return projectDir.wd;},
+					name: 'get/set wd',
 					test: {
 						assert: 'equal',
-						args: [[path.resolve(__dirname, '../')]]
+						args: [["/", path.resolve(__dirname, 'test-project')],
+							   ["/abcd", path.resolve(__dirname, 'test-project/abcd')],
+							   ["efgh", path.resolve(__dirname, 'test-project/abcd/efgh')],
+							   ["../", path.resolve(__dirname, 'test-project/abcd/')],
+							   ["/", path.resolve(__dirname, 'test-project/')],
+							   ["../", null]]
 					}
 				}
-
 			]
 		);
 	}
